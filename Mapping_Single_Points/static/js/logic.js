50fe2f4c5dd329a3a69b0e81c9e6208081adf5f3
CFlynn3 <cullinflynn@rocketmortgage.com>
2022-03-28 16:07:01 -0400

    finished 13.4.1 added circle around LA

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -10,9 +10,22 @@ console.log("working");
     // a geographical center, where the first coordinate is 
     // latitude(40.7) and the second is longitude(-94.5).
     // We set the zoom level of "4" on a scale 0–18.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+let map = L.map('mapid').setView([34.0522, -118.2437], 14);
+
+//  Add a marker to the map for Los Angeles, California.
+// let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+// Add a circle to the map for LA, CA
+L.circleMarker([34.0522, -118.2437], {
+    radius: 300,
+    color: "black",
+    fillColor: 'light-yellow'
+ }).addTo(map);
 
 // We create the tile layer that will be the background of our map.
+
+// Dark map view: 
+    // let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}'
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
