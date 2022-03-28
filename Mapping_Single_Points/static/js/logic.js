ee3a823f274e47a2d8a25fbb4e7c96b58cb94db4
CFlynn3 <cullinflynn@rocketmortgage.com>
2022-03-28 17:35:10 -0400

    added 13.4.3 flight route

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -10,52 +10,35 @@ console.log("working");
     // a geographical center, where the first coordinate is 
     // latitude(40.7) and the second is longitude(-94.5).
     // We set the zoom level of "4" on a scale 0–18.
-let map = L.map('mapid').setView([34.0522, -118.2437], 4);
+    let map = L.map('mapid').setView([37.6213, -122.3790], 5);
+
+// Coordinates for each point to be used in the line.
+let line = [
+    [33.9416, -118.4085],
+    [37.6213, -122.3790],
+    [40.7899, -111.9791],
+    [47.4502, -122.3088]
+  ];
+  
+// Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+    color: "yellow",
+    dashArray: "30 10",
+    weight: 5,
+    opacity: 0.5
+  }).addTo(map);
 
 //  Add a marker to the map for Los Angeles, California.
 // let marker = L.marker([34.0522, -118.2437]).addTo(map);
-
 // // Instead of 'marker', add an array containing each city's location, state, and population.
-// let cities = [{
-//     location: [40.7128, -74.0059],
-//     city: "New York City",
-//     state: "NY",
-//     population: 8398748
-//   },
-//   {
-//     location: [41.8781, -87.6298],
-//     city: "Chicago",
-//     state: "IL",
-//     population: 2705994
-//   },
-//   {
-//     location: [29.7604, -95.3698],
-//     city: "Houston",
-//     state: "TX",
-//     population: 2325502
-//   },
-//   {
-//     location: [34.0522, -118.2437],
-//     city: "Los Angeles",
-//     state: "CA",
-//     population: 3990456
-//   },
-//   {
-//     location: [33.4484, -112.0740],
-//     city: "Phoenix",
-//     state: "AZ",
-//     population: 1660272
-//   }
-// ];
-  
+// ((((We can also add this to another file))))
+
 // // Loop through the cities array and create one marker for each city.
 // cities.forEach(function(city) {
 //     console.log(city)
 //     L.marker(city.location).addTo(map);
 // });
 
-
-// ((((We can also add this to another file))))
 // Get data from cities.js
 let cityData = cities;
 
@@ -88,7 +71,7 @@ cityData.forEach(function(city) {
 
 // Dark map view: 
     // let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}'
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
