046159b8f500d863028b63f269750d905d6e646d
CFlynn3 <cullinflynn@rocketmortgage.com>
2022-03-28 22:49:30 -0400

    adding Toronto flight data

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -29,6 +29,12 @@ console.log("working");
         accessToken: API_KEY
     });
 
+    let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+        attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+            maxZoom: 18,
+            accessToken: API_KEY
+        });
+
 // Create the map object with center at the San Francisco airport.
 // let map = L.map('mapid').setView([37.5, -122.5], 10);
 
@@ -38,39 +44,39 @@ console.log("working");
 // Create a base layer that holds both maps.
 let baseMaps = {
     Street: streets,
-    Dark: dark
+    Dark: dark,
+    Light: light
 };
 
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-    center: [30, 30],
+    center: [44.0, -80.0],
     zoom: 2,
-    layers: [streets]
+    layers: [light]
 });
 
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
-// Add GeoJSON data.
-let sanFranAirport =
-{"type":"FeatureCollection","features":[{
-    "type":"Feature",
-    "properties":{
-        "id":"3469",
-        "name":"San Francisco International Airport",
-        "city":"San Francisco",
-        "country":"United States",
-        "faa":"SFO",
-        "icao":"KSFO",
-        "alt":"13",
-        "tz-offset":"-8",
-        "dst":"A",
-        "tz":"America/Los_Angeles"},
-        "geometry":{
-            "type":"Point",
-            "coordinates":[-122.375,37.61899948120117]}}
-]
-};
+// let sanFranAirport =
+// {"type":"FeatureCollection","features":[{
+//     "type":"Feature",
+//     "properties":{
+//         "id":"3469",
+//         "name":"San Francisco International Airport",
+//         "city":"San Francisco",
+//         "country":"United States",
+//         "faa":"SFO",
+//         "icao":"KSFO",
+//         "alt":"13",
+//         "tz-offset":"-8",
+//         "dst":"A",
+//         "tz":"America/Los_Angeles"},
+//         "geometry":{
+//             "type":"Point",
+//             "coordinates":[-122.375,37.61899948120117]}}
+// ]
+// };
 
 // Add it to the map:
 // L.geoJSON(sanFranAirport).addTo(map);
@@ -99,20 +105,20 @@ let sanFranAirport =
     // Then we add a marker for each feature in a return statement.
 
 // Coordinates for each point to be used in the line.
-let line = [
-    [33.9416, -118.4085],
-    [37.6213, -122.3790],
-    [40.7899, -111.9791],
-    [47.4502, -122.3088]
-  ];
+// let line = [
+//     [33.9416, -118.4085],
+//     [37.6213, -122.3790],
+//     [40.7899, -111.9791],
+//     [47.4502, -122.3088]
+//   ];
   
 // Create a polyline using the line coordinates and make the line red.
-L.polyline(line, {
-    color: "yellow",
-    dashArray: "30 10",
-    weight: 5,
-    opacity: 0.5
-  }).addTo(map);
+// L.polyline(line, {
+//     color: "yellow",
+//     dashArray: "30 10",
+//     weight: 5,
+//     opacity: 0.5
+//   }).addTo(map);
 
 //  Add a marker to the map for Los Angeles, California.
 // let marker = L.marker([34.0522, -118.2437]).addTo(map);
@@ -128,6 +134,12 @@ L.polyline(line, {
 
 // Get data from cities.js
 let cityData = cities;
+
+// Accessing the Toronto airline routes GeoJSON URL.
+let torontoData = "https://raw.githubusercontent.com/<GitHub_name>/Mapping_Earthquakes/main/torontoRoutes.json";
+
+// Accessing the airport GeoJSON URL (which can be generated directly on GitHub by clicking on a .json file you've pushed and viewed 'raw')
+let airportData = "https://raw.githubusercontent.com/pizzahut111/Earthquakes/main/majorAirports.json";
 
 cityData.forEach(function(city) {
     console.log(city)
@@ -153,23 +165,23 @@ cityData.forEach(function(city) {
 //     fillColor: 'light-yellow'
 //  }).addTo(map);
 
-
-
-// Accessing the airport GeoJSON URL (which can be generated directly on GitHub by clicking on a .json file you've pushed and viewed 'raw')
-let airportData = "https://raw.githubusercontent.com/pizzahut111/Earthquakes/main/majorAirports.json";
-
 // Puts markers for all json objects in 'airportData' (here 'data' references 'airportData')
-d3.json(airportData).then(function (data) {
+// d3.json(airportData).then(function (data) {
+//     console.log(data);
+//     // Creating a GeoJSON layer with the retrieved data.
+//     L.geoJSON(data)
+//         .bindPopup(function (layer) {
+//             return layer.feature.properties.name;
+//         })
+//         .addTo(map);
+// });
+
+// Toronto flights
+d3.json(torontoData).then(function(data) {
     console.log(data);
-    // Creating a GeoJSON layer with the retrieved data.
-    L.geoJSON(data)
-        .bindPopup(function (layer) {
-            return layer.feature.properties.name;
-        })
-        .addTo(map);
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJSON(data).addTo(map);
 });
-
-
 
 // To change the map's style, change the map id
 // using the list of Mapbox ids below:
